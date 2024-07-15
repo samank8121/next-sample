e77dec04de4216b36c7ff10c00964f2e1246d7e7
saman <saman.k8121@gmail.com>
2024-07-15 11:56:53 +0300

    chore: change translation structure

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,8 +5,11 @@ const locales: string[] = ['en', 'de'];
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
-
+  const messages = {
+    ...(await import(`../content/${locale}/home.json`)).default,
+    ...(await import(`../content/${locale}/product.json`)).default
+  };
   return {
-    messages: (await import(`../content/${locale}.json`)).default
+    messages
   };
 });
